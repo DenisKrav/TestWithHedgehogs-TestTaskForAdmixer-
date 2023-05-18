5925388a698db223d4e577c20515773d69523aaa
DenisKrav <den.325.2002.5@gmail.com>
2023-05-18 22:53:04 +0300

    Добавьте файлы проекта.

---
+++ TestWithHedgehogs(TestTaskForAdmixer)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestWithHedgehogs_TestTaskForAdmixer_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
